6618909dea7d3380c39647a53adf4b14e80eeeda
nikaixuan <nkx422@gmail.com>
2018-02-13 17:02:20 +1100

    finish todolist

--- JQuery Demo/ToDoListProject/assets/js/todo.js
+++ JQuery Demo/ToDoListProject/assets/js/todo.js
@@ -13,6 +13,10 @@ $("input[type = 'text']").keypress(function(event){
 	if (event.which === 13) {
 		var newTodo = $(this).val();
 		$(this).val("");
-		$("ul").append("<li><span> X </span>"+newTodo+"</li>"); 
+		$("ul").append("<li><span><i class=\"fas fa-trash\"></i></span>"+newTodo+"</li>"); 
 	}
+})
+
+$(".fa-plus").click(function(){
+	$("input[type = 'text']").fadeToggle();
 })
